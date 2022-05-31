6df2a233ef672ce016c644c52103c8a003c4dcfd
Eden Bourezg <uncelias@gmail.com>
2022-05-31 09:14:54 +0200

    bouton contact fonctionelle

--- src/components/btn_foot.vue
+++ src/components/btn_foot.vue
@@ -1,9 +1,17 @@
 <template>
-  <button
-    class="bg-gradient-to-t to-indigo-500 from-purple-500 rounded-xl text-white"
-  >
-    {{ btn }}
-  </button>
+  <RouterLink to="contact">
+    <button
+      class="
+        bg-gradient-to-t
+        to-indigo-500
+        from-purple-500
+        rounded-xl
+        text-white
+      "
+    >
+      {{ btn }}
+    </button>
+  </RouterLink>
 </template>
 
 <script>
