57b7ae1fcdc5f8bc3ac6a50794d0d2e7d20df760
Eden Bourezg <uncelias@gmail.com>
2022-05-31 10:19:30 +0200

    page mention légales

--- src/views/mentionslegales.vue
+++ src/views/mentionslegales.vue
@@ -1,3 +1,36 @@
+<template>
+  <h1
+    class="text-white font-space-age text-center mt-8 mb-8 mr-5 ml-5 text-4xl"
+  >
+    LEGAL MENTIONS
+  </h1>
+
+  <p class="text-white ml-5 mr-5 mb-5">
+    LEGAL NOTICES
+    <br /><br />
+    In accordance with the provisions of Law No. 2004-575 of June 21, 2004 on
+    confidence in the digital economy, users of the Utopia site are informed of
+    the identity of the various parties involved in its creation and monitoring.
+    <br /><br />
+    Edition of the site
+    <br /><br />
+    This site, accessible at the URL www.utopiafest.com (the "Site"), is
+    published by : Utopiafest, a company with a capital of 1000000 euros,
+    registered at the R.C.S. of PARIS under the number 00000000, whose head
+    office is located at 4 Place Tharradin, 25200 Montbéliard, represented by
+    Eden Bourezg duly empowered
+    <br /><br />
+    The individual VAT number of the publisher is : 00000000.
+    <br /><br />
+    Hosting The Site is hosted by the company Infomaniak, located at Rue
+    Eugène-Marziano 25, 1227 Geneva, Switzerland, (telephone contact or email:
+    (+41) 22 820 35 44).
+    <br /><br />
+    Director of publication The Director of the publication of the Site is Eden
+    Bourezg. To contact us By phone : +33381994734 By email :
+    bourezg.eden@gmail.com By mail : 4 Place Tharradin, 25200 Montbéliard
+  </p>
+</template>
 <script >
 import card from "../components/card.vue";
 import card2 from "../components/card2.vue";
